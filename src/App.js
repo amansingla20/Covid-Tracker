ebd05e2258f214faf2def752eed06b9443d5c8d8
AMAN SINGLA <amansingla222@gmail.com>
2021-04-30 10:52:17 +0530

    Started Implementing Header of Covid-Tracker Application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
   return (
     <div className="App">
-      <div></div>
+      <div className="App-header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app-dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
